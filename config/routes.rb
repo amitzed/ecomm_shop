507b875e4b6768131466c779a7353af4bdf24fb6
Amit <amit@AZ-iMac.local>
2018-08-17 21:50:30 -0400

    in routes.rb changed store#index to items#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     registrations: 'registrations'
   }
   # below is the home page
-  root 'store#index'
+  root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
