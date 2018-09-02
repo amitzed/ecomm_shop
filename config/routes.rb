2fd860fe0a578eac6abb39e89d3f998186ff01d9
Amit <amit@AZ-iMac.local>
2018-09-02 17:07:32 -0400

    More heroku deployment trouble shooting

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   }
   # below is the home page
   root 'items#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
