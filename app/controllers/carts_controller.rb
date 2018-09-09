1017e4a3ad2ef9d9ec52d63047f1131c792cf2e7
Amit <amit@AZ-iMac.local>
2018-09-08 20:03:24 -0400

    Styling2

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -21,7 +21,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart is Now Created!' }
+        format.html { redirect_to @cart, notice: 'Basket is Now Created!' }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -33,7 +33,7 @@ class CartsController < ApplicationController
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Your Cart Has Been Updated.' }
+        format.html { redirect_to @cart, notice: 'Your Basket Has Been Updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit }
@@ -46,7 +46,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Your Cart Has Been Emptied' }
+      format.html { redirect_to root_path, notice: 'Your Basket Has Been Dumped!' }
       format.json { head :no_content }
     end
   end
@@ -63,6 +63,6 @@ class CartsController < ApplicationController
 
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to root_path, notice: "That cart does not exist"
+      redirect_to root_path, notice: "That basket does not exist"
     end
 end
